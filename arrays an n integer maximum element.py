619c7bfde73db698007c258d6e38067d42661a65
Aswitha.T <42570764+AswiAs@users.noreply.github.com>
2019-01-11 10:32:27 +0530

    Create arrays an n integer maximum element.py

---
+++ arrays an n integer maximum element.py
@@ -0,0 +1,7 @@
+{ 
+    int arr[] = {10, 324, 45, 90, 9808}; 
+    int n = sizeof(arr) / sizeof(arr[0]); 
+    cout << "Largest in given array is " 
+         << largest(arr, n); 
+    return 0; 
+} 
